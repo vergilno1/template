1a8473aa44b44f1e600cc3c7397399084465509d
vergilno1 <33945986+vergilno1@users.noreply.github.com>
2021-11-17 10:38:45 +0800

    Create code.py

---
+++ code.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import seaborn as sns
+import numpy as np
+
+from PIL import Image #"pip install pillow" for PIL package
+import glob
+from collections import defaultdict
+from tensorflow import keras
+from tensorflow.keras import layers
